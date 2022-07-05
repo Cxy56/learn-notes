0c92d092822f760ad642bffde148e59c15b2b6bc
cherrychen <820769835@qq.com>
2022-07-05 15:51:29 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "前端学习笔记",
+  description: "Just playing around",
+  // base: "/bar/", // 网站部署到 https://foo.github.io/bar/下访问
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "vue3", link: "/vue3/" },
+      { text: "webpack", link: "/webpack/" },
+      { text: "node", link: "/node/" },
+      { text: "qiankun", link: "/qiankun/" },
+    ],
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@alias": "path/to/some/dir", // ![Image from alias](~@alias/image.png)
+      },
+    },
+  },
+}
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+首页
+---
+
+Hello!
+
+学习笔记计划放在这里
+
+![An image](./static/img/fengjing.jpeg)
+
